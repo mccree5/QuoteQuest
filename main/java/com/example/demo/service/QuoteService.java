456cbb6e0dc8433c2ee8b6511ede25f933896ec4
mccree5 <155380854+mccree5@users.noreply.github.com>
2024-03-19 20:21:31 -0500

    Delete main directory

--- main/java/com/example/demo/service/QuoteService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-import java.util.List;
-public interface QuoteService {
-String getRandomQuote();
-    List<Quote> getAllQuotes();
-
-    void addQuote(Quote quote);
-}
